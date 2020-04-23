ccfb145ab3cb2df06d15a153bdb5eede123a0718
Michael Adkins <mikeadkins1112@gmail.com>
2020-04-23 23:34:50 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,13 +3,11 @@
 
 def key_for_min_value(name_hash)
   return nil if name_hash == {}
-    #return nil
-  #else
-    x=1000
+    lowest_value = nil
     new_hash = nil
     name_hash.each do |name, number|
-      if x >  number
-        x = number
+      if lowest_value == nil || lowest_value >  number
+        lowest_value = number
         new_hash = name
       end
     end
